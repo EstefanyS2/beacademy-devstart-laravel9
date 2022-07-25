886a9cfef0806345e1d365d6136e458219689a7f
Estefany Alessandra <k.estefanyalessandra@gmail.com>
2022-07-25 10:42:21 -0300

    testes

--- example-app/routes/web.php
+++ example-app/routes/web.php
@@ -15,11 +15,11 @@ Route::get('/posts', [PostController::class, 'index'])->name('posts.index');
 Route::get('/users/{id}/posts', [PostController::class, 'show'])->name('posts.show');
 
 
-Route::delete('/users/{id}', [UserController::class, 'destroy'])->name('users.destroy')->middleware('auth');
-Route::put('/users/{id}', [UserController::class, 'update'])->name('users.update')->middleware('auth');
-Route::get('/users/{id}/edit', [UserController::class, 'edit'])->name('users.edit')->middleware('auth');
-Route::get('/users', [UserController::class, 'index'])->name('users.index')->middleware('auth');
-Route::get('/users/create', [UserController::class, 'create'])->name('users.create')->middleware('auth');
+Route::delete('/users/{id}', [UserController::class, 'destroy'])->name('users.destroy');
+Route::put('/users/{id}', [UserController::class, 'update'])->name('users.update');
+Route::get('/users/{id}/edit', [UserController::class, 'edit'])->name('users.edit');
+Route::get('/users', [UserController::class, 'index'])->name('users.index');
+Route::get('/users/create', [UserController::class, 'create'])->name('users.create');
 Route::post('/users', [UserController::class, 'store'])->name('users.store');
 Route::get('/users/{id}', [UserController::class, 'show'])->name('users.show');
 
